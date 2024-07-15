c1fadeed54a7921f043bb309f2a21d182082527a
Vlad1m1r1337 <vladim1r.gribkov@yandex.ru>
2024-07-16 00:49:24 +0400

    done engine

---
+++ index.js
@@ -0,0 +1,40 @@
+import templateEngine from "./engine.js";
+
+const block = {
+    tag: 'div',
+    cls: ['container', 'main'],
+    attrs: { id: 'Div', 'data-role': 'content' },
+    content: [
+        'Hello, World!',
+        { tag: 'span', content: 'This is a span.' }
+    ]
+};
+
+const block1 = {
+    tag: 'div',
+    cls: ['container', 'main'],
+    attrs: { id: 'mainDiv', 'data-role': 'content' },
+    content: [
+        'WORLD',
+        { tag: 'span', content: 'SPAN' }
+    ]
+};
+
+const element = templateEngine(block);
+const element1 = templateEngine(block1);1
+document.body.appendChild(element);
+
+const add = document.getElementById('add');
+
+add.addEventListener('click', () => {
+	console.log('add');
+	document.body.appendChild(element1);
+})
+
+const del = document.getElementById('delete');
+
+del.addEventListener('click', () => {
+	console.log('del');
+	const div = document.getElementById('Div');
+	document.body.removeChild(div);
+})
