7aa7d2f28254b042241e1d263e1f86c6f5cc6863
vladimir <vladim1r.gribkov@yandex.ru>
2024-12-18 20:02:25 +0400

    add cors and get

--- front/pages/history.js
+++ front/pages/history.js
@@ -3,7 +3,7 @@ import showToast from "../toast.js";
 
 const fetchHistroy = async () => {
     try {
-        const response = await fetch('../mock/history.json');
+        const response = await fetch('http://127.0.0.1:8000/api/clicker-matches/');
         if (!response.ok) {
             throw new Error('Network response was not ok');
         }
