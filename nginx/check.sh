f78385740185f9a8140e354216121342e246a5d4
olnytim <yearagotomorrow@gmail.com>
2024-12-20 18:18:37 +0000

    trying to commit everything up

--- nginx/check.sh
+++ nginx/check.sh
@@ -9,11 +9,16 @@ RED="\033[0;31m"
 
 # 'TCP:front:3000': This specifies the target address and port to connect to
 
-until socat - TCP:front:5000; do
-    echo -e "${RED}Front isn't up...waiting..."
+until socat - TCP:front:5000 && socat - TCP:back:5001; do
+    if ! socat - TCP:front:5000; then
+        echo -e "${RED}Front isn't up...waiting..."
+    fi
+    if ! socat - TCP:back:5001; then
+        echo -e "${RED}Back isn't up...waiting..."
+    fi
     sleep 4
 done
 
-echo -e "${PURPLE}Front is up!"
+echo -e "${PURPLE}Both Front and Back are up!"
 
 nginx -g "daemon off;"
